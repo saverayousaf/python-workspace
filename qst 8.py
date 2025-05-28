26e347404edbb2c7824d26c73888752e51e7cacf
saverayousaf <saverayousaf72@gmail.com>
2025-05-28 22:17:29 +0500

    Update qst 8.py

--- qst 8.py
+++ qst 8.py
@@ -1,15 +1,10 @@
 # Q8: Tuple marks operations
 marks = (75, 80, 65, 90, 85)
-
-
 print("First mark:", marks[0])
 print("Last mark:", marks[-1])
 
-
 m1, m2, m3, m4, m5 = marks
 print("Unpacked marks:", m1, m2, m3, m4, m5)
-
-
 new_marks = tuple(mark + 5 for mark in marks)
 print("Original marks:", marks)
 print("New marks (+5):", new_marks)
